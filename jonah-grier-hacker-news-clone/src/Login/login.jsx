d4e7f469cc31d3e6a75e67f37b307359fdc6f9c1
grierJonah <jjg8895@gmail.com>
2021-03-29 13:45:23 -0700

    Modified style of login screen

--- jonah-grier-hacker-news-clone/src/Login/login.jsx
+++ jonah-grier-hacker-news-clone/src/Login/login.jsx
@@ -48,42 +48,43 @@ class Register extends React.Component {
 
 	render() {
 		return (
-			<div className="App">
-				<div className="container">
-					<div className="form-group">
-						<form onSubmit={this.sendToDatabase.bind(this)}>
-							<div class="form-group">
-								<label for="username">Username:</label>
-								<input
-									type="text"
-									placeholder="Username"
-									onChange={this.changeUsername}
-									value={this.state.username}
-									className="form-control form-group"
-								/>
-							</div>
-							<div lass="form-group">
-								<input
-									type="password"
-									placeholder="Password"
-									minLength="8"
-									onChange={this.changePassword}
-									value={this.state.password}
-									className="form-control form-group register"
-								/>
-							</div>
-							<small id="login-help" class="form-text text-muted">
-								Enter your username and password to login!
-							</small>
-							<div class="form-group" id="register-outer-button">
-								<input
-									type="submit"
-									className="btn btn-primary"
-									value="Login"
-								/>
-							</div>
-						</form>
-					</div>
+			<div className="container">
+				<div className="form-group">
+					<form onSubmit={this.sendToDatabase.bind(this)}>
+						<div class="form-group row">
+							<label htmlFor="username">Username:</label>
+							<input
+								type="text"
+								placeholder="Username"
+								onChange={this.changeUsername}
+								value={this.state.username}
+								className="form-control form-group"
+								required={true}
+							/>
+						</div>
+						<div class="form-group row">
+							<label htmlFor="username">Password:</label>
+							<input
+								type="password"
+								placeholder="Password"
+								minLength="8"
+								onChange={this.changePassword}
+								value={this.state.password}
+								className="form-control form-group register"
+								required={true}
+							/>
+						</div>
+						<small id="login-help" class="form-text text-muted">
+							Enter your username and password to login!
+						</small>
+						<div class="form-group" id="register-outer-button">
+							<input
+								type="submit"
+								className="btn btn-primary"
+								value="Login"
+							/>
+						</div>
+					</form>
 				</div>
 			</div>
 		);
